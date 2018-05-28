f085490656a95732ef182b1fff0139d1b41ae9a0
mahmudulshuvo <shuvo0201@gmail.com>
2018-05-28 15:13:32 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular';
+  sa = '';
+  ba = '';
+
+  copyText() {
+    if (!this.sa) {
+      document.getElementById("demo").innerHTML = "Please input text!"
+    }
+    else {
+      document.getElementById("demo").innerHTML = "Copy text!"
+      this.ba = this.sa;
+    }
+  }
+
+  clearText() {
+    document.getElementById("demo").innerHTML = "Clear text!"
+    this.sa = undefined;
+    this.ba = undefined;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms'; 
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
