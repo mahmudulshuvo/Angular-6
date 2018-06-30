d703a3a4745ea1b74a3e82ae05f97f0c524150eb
mahmudulshuvo <shuvo0201@gmail.com>
2018-06-30 17:36:50 +0600

    ShowHideBtn property removed

--- src/app/testui/testui.component.ts
+++ src/app/testui/testui.component.ts
@@ -28,7 +28,7 @@ export class TestuiComponent implements OnInit {
   constructor(private data: DataService) { }
 
   ngOnInit() {
-    console.log('New UI');
+    console.log('New UI Request');
   }
 
   onSubmit() {
