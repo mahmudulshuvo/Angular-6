306d0553fce7246caabbd400a53bf60941cb57fd
mahmudulshuvo <shuvo0201@gmail.com>
2018-06-30 17:33:00 +0600

    New UI Request with clean code

--- src/app/replay/replay.component.ts
+++ src/app/replay/replay.component.ts
@@ -8,28 +8,6 @@ import { Component, OnInit, Input } from '@angular/core';
 
 export class ReplayComponent implements OnInit {
 
-  // @Input() name: string;
-  // public selectedDate: Date;
-
-  // first = '';
-  // second = '';
-  // date = '';
-  // constructor() {
-  // }
-
-
-  // ngOnInit() {
-  //   console.log('In replay');
-  // }
-
-  // onClickButton() {
-  //   this.first = (<HTMLInputElement>document.getElementById("first")).value;
-  //   this.second = (<HTMLInputElement>document.getElementById("second")).value;
-  //   var mainTextArea = document.getElementById("mainTextArea");
-  //   mainTextArea.innerHTML = 'Input 1: ' + this.first + '\n' + "Input 2: " + this.second + '\n' + "Date: " + this.selectedDate;
-  //   console.log(this.selectedDate);
-  // }
-
   @Input() name: string;
   public selectedDate: Date;
 
